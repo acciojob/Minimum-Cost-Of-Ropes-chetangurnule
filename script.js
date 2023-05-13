6e27c4e42c2b8adeb0e477d055a9855f02e58bec
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-13 17:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+    //your code here
+    let ropes = document.querySelector("#rope-lengths").value;
+    let result = document.querySelector("#result");
+      let arr = ropes.split(" ");
+      let sum = 0;
+      while(arr.length > 1){
+        arr.sort((a, b)=> a-b);
+        let a = arr.shift();
+        let b = arr.shift();
+        let c = +a + +b;
+        sum += c;
+        arr.push(c);
+      }
+      result.textContent = sum;
+  } 
